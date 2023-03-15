a30da032f8140e675827e5ad1129237e2be1d937
vladbochok <vladbochok1@gmail.com>
2023-03-15 19:05:27 +0200

    Use correct event

--- l2/contracts/bridge/interfaces/IL2StandardToken.sol
+++ l2/contracts/bridge/interfaces/IL2StandardToken.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 interface IL2StandardToken {
-    event BridgeInitialization(address indexed l1Token, string name, string symbol, uint8 decimals);
+    event BridgeInitialize(address indexed l1Token, string name, string symbol, uint8 decimals);
 
     event BridgeMint(address indexed _account, uint256 _amount);
 
