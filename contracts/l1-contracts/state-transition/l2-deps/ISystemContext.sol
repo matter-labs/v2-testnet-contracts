f47ed343d0681fea4ae744c6834300ef1e2b9d63
Bence Haromi <bence.haromi@gmail.com>
2024-08-21 11:12:55 +0100

    Revert "Added contracts from https://github.com/matter-labs/era-contracts.git (protocol-defense - 9aab5b864693c24ae3240a4a8370d1a3d855c08a)"
    This reverts commit 2ebd78b15f1b98245fef4dbdd74103c57ab7fe81.

--- contracts/l1-contracts/state-transition/l2-deps/ISystemContext.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-// We use a floating point pragma here so it can be used within other projects that interact with the zkSync ecosystem without using our exact pragma version.
-pragma solidity ^0.8.21;
-
-interface ISystemContext {
-    function setChainId(uint256 _newChainId) external;
-}
