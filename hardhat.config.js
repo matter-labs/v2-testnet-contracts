9a257c1d72197551873b08b5ea51c3196a2790ca
Bence Haromi <bence.haromi@gmail.com>
2024-08-28 14:05:50 +0100

    chore(hardhat.config.js): 0.8.24 compiler only

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,14 @@
 module.exports = {
   solidity: {
-    compilers: [
-      {
-        version: "0.8.24",
-        settings: {
-          evmVersion: "cancun",
-          viaIR: process.env.HARDHAT_VIA_IR === "true",
-          optimizer: {
-            enabled: true,
-            runs: 9999999,
-          },
-        },
+    version: "0.8.24",
+    settings: {
+      evmVersion: "cancun",
+      viaIR: process.env.HARDHAT_VIA_IR === "true",
+      optimizer: {
+        enabled: true,
+        runs: 9999999,
       },
-      {
-        version: "0.8.20",
-        settings: {
-          viaIR: process.env.HARDHAT_VIA_IR === "true",
-          optimizer: {
-            enabled: true,
-            runs: 9999999,
-          },
-        },
-      },
-    ],
+    },
   },
   paths: {
     sources: process.env.HARDHAT_CONTRACTS_PATH || "contracts",
