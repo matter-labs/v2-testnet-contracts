c6076fa57f1edb5d8e3ae37905f50b5579855540
Bence Haromi <56651250+benceharomi@users.noreply.github.com>
2024-08-28 10:25:19 +0100

    feat: package revamp (#24)
    * outdated contracts removed
    
    * hardhat config removed
    
    * dependencies removed
    
    * feat: package revamp part 2 (setup) (#25)
    
    * `hardhat.config.js` added
    
    * `foundry.toml` added
    
    * `fetch_contracts` script added
    
    * system-contracts preprocessing added to `fetch_contracts` script
    
    * Added contracts from https://github.com/matter-labs/era-contracts.git (protocol-defense - 9aab5b864693c24ae3240a4a8370d1a3d855c08a)
    
    * openzeppelin dependencies (`contracts-v4` and `contracts-upgradeable-v4`) added
    
    * 0.8.20 added to hardhat compiler list
    
    * foundry auto solidity version detection enabled
    
    * hardhat ci
    
    * foundry ci
    
    * hardhat ci typo
    
    * ci rework
    
    * ci yml to yaml
    
    * ci fix
    
    * shortened job names in ci
    
    * Revert "Added contracts from https://github.com/matter-labs/era-contracts.git (protocol-defense - 9aab5b864693c24ae3240a4a8370d1a3d855c08a)"
    
    This reverts commit 79798f3ec9bf96dab8e35c1e54a47347547605d4.
    
    * unnecessary remappings.txt removed
    
    * forge install: openzeppelin-contracts-v4
    
    v4.9.5
    
    * forge install: openzeppelin-contracts-upgradeable-v4
    
    v4.9.5
    
    * changing to submodule deps for foundry
    
    * yarn removed from foundry ci
    
    * foundry ci syntax fix
    
    * fetch target branch changed to dev
    
    * ref as input for fetch_contracts
    
    * update-contracts workflow
    
    * fetch_contracts usage message extended
    
    * formatting
    
    * default value of ref removed from workflow
    
    * populated package.json
    
    * added logo.svg
    
    * added LICENSE-MIT
    
    * readme updated
    
    * added installation and usage sections to readme
    
    * package.json repository section updated
    
    * publish-package workflow added
    
    * ci: via-ir flag set for l2 build jobs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,32 @@
 module.exports = {
-    solidity: {
-      version: "0.8.19",
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 1000,
-        }
-      }
-    },
-    paths: {
-      sources: "./l1",
-      cache: "./cache",
-      artifacts: "./artifacts"
-    },
-    mocha: {
-      timeout: 40000
-    }
-}
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          evmVersion: "cancun",
+          viaIR: process.env.HARDHAT_VIA_IR === "true",
+          optimizer: {
+            enabled: true,
+            runs: 9999999,
+          },
+        },
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          viaIR: process.env.HARDHAT_VIA_IR === "true",
+          optimizer: {
+            enabled: true,
+            runs: 9999999,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: process.env.HARDHAT_CONTRACTS_PATH || "contracts",
+    artifacts: "artifacts",
+    cache: "cache",
+  },
+};
